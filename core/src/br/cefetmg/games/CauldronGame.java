2783813773da50a81434b751091eb90263a89c4d
Sarah Rodrigues Moreira <sarahrodriguesmoreira@gmail.com>
2017-10-20 00:28:05 -0200

    adicionando a poção do amor

--- core/src/br/cefetmg/games/CauldronGame.java
+++ core/src/br/cefetmg/games/CauldronGame.java
@@ -112,9 +112,12 @@ public class CauldronGame extends ApplicationAdapter {
 
         // instancia, configura e dá início ao efeito das bolhas
         // use o campo ParticleEffect bolhas definido na linha #38
-        // ...
-        // ...
-        // ...
+        // pocao do amor //
+        bolhas = ((ParticleEffect) assets.get("bolhas.pfx")).copy();
+        bolhas.init();
+        bolhas.start();
+        bolhas.translate(new Vector3(0, 1f, 0));
+        sistemaParticulas.add(bolhas);
         
         // começa a música
         musica.setLooping(true);
